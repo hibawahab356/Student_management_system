5c184a172525111610f9c98980d81abd5022ae99
Alisha432 <alishu432@mail.com>
2026-08-17 23:42:55 +0500

    pending

--- app.js
+++ app.js
@@ -10,9 +10,86 @@ console.log(client);
 
 const form = document.querySelector("#studentRegisteration")
 
-form.addEventListener("submit",(event)=>{
-    event.preventDefault()
+form.addEventListener("submit",async(event)=>{
+try{
+        event.preventDefault()
     const formInfo = new FormData(form)
+
+   let emptyField = false 
+
+let inputs = document.querySelectorAll("input")
+
+
+inputs.forEach((input)=>{
+    if(input.value === ""){
+        input.style.border = "2px solid red"
+        emptyField = true
+    }
+})
+
+if(emptyField){
+    return
+}
+
+
     const data = Object.fromEntries(formInfo)
-    console.log(data);
+    const {email,password,
+firstName,lastName
+,fatherName,address,city,course,gender,dob} = data
+console.log(firstName,lastName,course,city,dob,fatherName,address,gender);
+
+
+    const { data:signUpData, error } = await client.auth.signUp({
+  email,
+  password,
+})
+
+const id = signUpData?.user?.id
+console.log(id);
+
+// database insertion
+
+const { error:databaseError } = await client
+  .from('students_data')
+  .insert({firstName,
+    lastName,
+"father'sName": fatherName,
+address,
+city,
+course,
+gender,
+dob,
+user_id : id,
+  })
+
+console.log(databaseError);
+
+
+if(signUpData){
+    console.log(signUpData);
+    
+}else{
+    console.log(error.message);
+    
+}
+
+}
+
+catch(error){
+    console.log(error);
+    
+}
+})
+
+
+let inputs = document.querySelectorAll("input")
+
+inputs.forEach((input)=>{
+    input.addEventListener("input",()=>{
+ if(input.value !== ""){
+        input.style.border = ""
+        
+    }
+    })
+   
 })
